ba611356d869ae19fa85f0f62fad8f1759a35c0f
Alexc73 <127415658+Alexc73@users.noreply.github.com>
2023-05-03 20:59:21 +0100

    flip card + toggle

--- src/App.js
+++ src/App.js
@@ -4,29 +4,52 @@ import './App.css';
 import Register from './components/register/Register';
 import Login from './components/login/Login.js'
 import TodoList from './components/Todo/TodoList';
+// import Toggle from './components/Toggle/Toggle';
 
 import { useState, useEffect } from 'react';
 
 const App = () => {
 
   const [user, setUser] = useState()
+  const [isFlipped, setIsFlipped] = useState(false);
 
+  const handleToggle = () => {
+    setIsFlipped(!isFlipped);
+  };
+  
   return (
-    <div className="container">
-            {user 
-      ?
-      <h2>Hello welcome {user} you have logged in</h2>
-      :
-      <h2>Please log in</h2>
-
-      }
-
-      <Register newUser={setUser}/>
-      <Login newUser={setUser}/>
-      <TodoList />
+    <div>
+      <div className="toggle-container">
+        <button className={!isFlipped ? "active" : ""} onClick={() => setIsFlipped(false)}>Register</button>
+        <h3>OR </h3>
+        <button className={isFlipped ? "active" : ""} onClick={() => setIsFlipped(true)}>Login</button>
       </div>
+      {/* <div className="toggle-container">
+        <Toggle />
+      </div> */}
+      <div className="container">
+        <div className={`card-container ${isFlipped ? 'flipped' : ''}`}>
+          <div className="card">
+            <div className="front">
+              <Register newUser={setUser} />
+            </div>
+            <div className="back">
+              <Login newUser={setUser} />
+            </div>
+          </div>
+        </div>
+        {user 
+          ?
+          <h1>Hello welcome {user} you have logged in</h1>
+          :
+          <h1>Please log in</h1>
+        }
+        <div className="todo-container">
+          <TodoList />
+        </div>
+      </div>
+    </div>
   );
 }
 
 export default App;
-
---
+++ src/components/Toggle/Toggle.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import "../Toggle/Toggle.css";
+
+function Toggle() {
+
+  return (
+ <h1></h1>
+  );
+}
+
+export default Toggle;
