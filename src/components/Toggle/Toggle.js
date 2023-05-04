bbd32ce55ef8e1812d14d6f034304c156f579d96
Alexc73 <127415658+Alexc73@users.noreply.github.com>
2023-05-04 17:29:27 +0100

    toggle is flipped when buttons are pressed

--- src/components/Toggle/Toggle.js
+++ src/components/Toggle/Toggle.js
@@ -1,17 +1,15 @@
 import { useState } from 'react';
 import './Toggle.css';
 
-const Toggle = ({ onToggle }) => {
-  const [isChecked, setIsChecked] = useState(false);
-
+const Toggle = ({ onToggle, isFlipped, setIsFlipped }) => {
   const handleToggle = () => {
-    setIsChecked(!isChecked);
-    onToggle && onToggle(!isChecked);
+    setIsFlipped(!isFlipped);
+    onToggle && onToggle(!isFlipped);
   };
 
   return (
     <div>
-      <input type="checkbox" id="toggle" checked={isChecked} onChange={handleToggle} />
+      <input type="checkbox" id="toggle" checked={isFlipped} onChange={handleToggle} />
       <label htmlFor="toggle"></label>
     </div>
   );
